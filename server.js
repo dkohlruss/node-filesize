a806951e5a3b46c3af8d37c0562b51ec0e2da959
dkohlruss <dkohlruss@gmail.com>
2017-03-16 09:28:42 -0600

    put encoding in the wrong spot on the form like a dummy, fixed now

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const upload = multer({ dest: 'uploads/' });
 
 app.use(express.static(__dirname + '/public'));
 
-app.post('/', upload.single('file'), (req, res) => {
-  // req.file is the file file
-  let fileSize = req.file; //req.file is undefined
+app.post('/', upload.single('file'), (req, res, next) => {
+  // req.file is supposed to be the 'file' file
+  let fileSize = req.file.size; //req.file is undefined
   console.log(req.file);
   console.log(req.body); // req.body is also undefined
 
-  res.send({fileSize});
+  res.send({size: fileSize});
 });
 
 app.listen(port, () => {
